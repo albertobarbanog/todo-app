720f810841314e1606b257210285fca9f0c96b69
albertobarbanog <albertobarbanog@gmail.com>
2023-11-29 15:34:09 -0300

    Added gui

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Reads todos from file and returns them as a list"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Writes todos to file"""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__name__":
+    print("Hello")
+    print(get_todos())
